62cbb3b6d0b30ece4cf7c6025f6c82346f481fb2
Weckyy702 <weckyy702@gmail.com>
2021-11-06 21:02:08 +0100

    Moved is_* functions into Alphabet header

--- Lexer/include/Alphabet.h
+++ Lexer/include/Alphabet.h
@@ -30,7 +30,7 @@ namespace RaychelScript {
                 (c == TokenType::ampersand) ||
                 (c == TokenType::pipe) || 
                 (c == TokenType::caret);
-        // clang-format off
+        // clang-format on
     }
 
     [[nodiscard]] inline bool is_identifier_char(char c)
@@ -41,6 +41,74 @@ namespace RaychelScript {
     [[nodiscard]] inline bool is_number_char(char c)
     {
         return std::isdigit(static_cast<unsigned char>(c)) != 0;
+    }
+
+
+
+    /**
+    * \brief Check if an operator token falls into the MD part of PEMDAS
+    * 
+    * \param token token to check
+    * \return
+    */
+    [[nodiscard]] static bool is_MD_op(TokenType::TokenType type) noexcept
+    {
+        return (type == TokenType::star) || (type == TokenType::slash) || (type == TokenType::caret);
+    }
+
+    /**
+    * \brief Check if an operator token falls into the AS part of PEMDAS
+    * 
+    * \param token token to check
+    * \return
+    */
+    [[nodiscard]] inline bool is_AS_op(TokenType::TokenType type) noexcept
+    {
+        return (type == TokenType::plus) || (type == TokenType::minus);
+    }
+
+    /**
+    * \brief Check if a token is an arithmetic operator
+    * 
+    * \param token token to check
+    * \return
+    */
+    [[nodiscard]] inline bool is_arith_op(TokenType::TokenType type) noexcept
+    {
+        return is_MD_op(type) || is_AS_op(type);
+    }
+
+    /**
+    * \brief Check if a token is a closing parenthesis
+    * 
+    * \param type token to check
+    * \return
+    */
+    [[nodiscard]] inline bool is_closing_parenthesis(TokenType::TokenType type) noexcept
+    {
+        return (type == TokenType::right_paren) || (type == TokenType::right_bracket) || (type == TokenType::right_curly);
+    }
+
+    /**
+    * \brief Check if a token is an opening parenthesis
+    * 
+    * \param type token to check
+    * \return
+    */
+    [[nodiscard]] inline bool is_opening_parenthesis(TokenType::TokenType type) noexcept
+    {
+        return (type == TokenType::left_paren) || (type == TokenType::left_bracket) || (type == TokenType::left_curly);
+    }
+
+    /**
+    * \brief Check if a token is a parenthesis
+    * 
+    * \param type token to check
+    * \return
+    */
+    [[nodiscard]] inline bool is_parenthesis(TokenType::TokenType type) noexcept
+    {
+        return is_opening_parenthesis(type) || is_closing_parenthesis(type);
     }
 
 } // namespace RaychelScript
