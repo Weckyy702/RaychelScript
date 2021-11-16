d6ccf66af77ecedc2b2684fb9e5258fc19c8cfbe
Weckyy702 <50154363+Weckyy702@users.noreply.github.com>
2021-11-16 11:26:10 +0100

    Added VariableDescriptor class
    This class is a container for a variable with a unique identifier. It
    will be used during execution

---
+++ shared/include/Execution/VariableDescriptor.h
@@ -0,0 +1,32 @@
+/*DONT FORGET THE COPYRIGHT ;)*/
+#ifndef RAYCHELSCRIPT_VARIABLE_DESCRIPTOR_H
+#define RAYCHELSCRIPT_VARIABLE_DESCRIPTOR_H
+
+#include <concepts>
+
+namespace RaychelScript {
+
+    template <std::floating_point T>
+    class VariableDescriptor
+    {
+        static std::size_t s_id;
+
+    public:
+
+        VariableDescriptor()
+            :id_{s_id++}
+        {}
+
+        [[nodiscard]] std::size_t id() const noexcept
+        {
+            return id_;
+        }
+
+    private:
+        std::size_t id_;
+        T value_{0};
+    };
+
+} //namespace RaychelScript
+
+#endif //!RAYCHELSCRIPT_VARIABLE_DESCRIPTOR_H
