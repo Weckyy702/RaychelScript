98ba77aa5d1dbe15d133ba3f5b88c71491cb6704
Weckyy702 <weckyy702@gmail.com>
2021-11-23 08:53:08 +0100

    Made the Interpreter build on windows
    Mainly fixed dependecies and fixed DLL-linkage

--- Interpreter/include/Interpreter.h
+++ Interpreter/include/Interpreter.h
@@ -28,10 +28,22 @@
 #ifndef RAYCHELSCRIPT_INTERPRETER_H
 #define RAYCHELSCRIPT_INTERPRETER_H
 
+
+#ifdef _WIN32
+    #ifdef RaychelScriptInterpreter_EXPORTS
+        #define RAYCHELSCRIPT_INTERPRETER_API __declspec(dllexport)
+    #else
+        #define RAYCHELSCRIPT_INTERPRETER_API __declspec(dllimport)
+    #endif
+#else
+    #define RAYCHELSCRIPT_INTERPRETER_API
+#endif
+
 #include <concepts>
 #include <map>
 #include <string>
 #include <variant>
+#include <optional>
 
 #include "AST/AST.h"
 #include "ExecutionErrorCode.h"
@@ -40,32 +52,58 @@
 
 #include "RaychelCore/AssertingGet.h"
 
-namespace RaychelScript::Interpreter {
+namespace RaychelScript{
 
-    template <std::floating_point T>
-    using ExecutionResult = std::variant<InterpreterErrorCode, ExecutionState<T>>;
+    RAYCHELSCRIPT_INTERPRETER_API [[nodiscard]] std::string
+    get_descriptor_identifier(const std::map<std::string, DescriptorID>& descriptor_table, const DescriptorID& id) noexcept;
 
-    template <std::floating_point T>
-    using ParameterMap = std::map<std::string, T>;
-
-    [[nodiscard]] ExecutionResult<double> interpret(const AST& ast, const ParameterMap<double>& parameters) noexcept;
-
-    [[nodiscard]] inline ExecutionResult<double>
-    interpret(std::istream& source_stream, const ParameterMap<double>& parameters) noexcept
+    template <typename T>
+    [[nodiscard]] std::string get_descriptor_identifier(const ExecutionState<T>& state, const DescriptorID& id) noexcept
     {
-        const auto ast_or_error = Parser::parse(source_stream);
-        if (const auto* ec = std::get_if<Parser::ParserErrorCode>(&ast_or_error); ec) {
-            return InterpreterErrorCode::no_input;
-        }
-
-        return interpret(Raychel::get<AST>(ast_or_error), parameters);
+        return get_descriptor_identifier(state._descriptor_table, id);
     }
 
-    [[nodiscard]] inline ExecutionResult<double>
-    interpret(const std::string& source_text, const ParameterMap<double>& parameters) noexcept
+    template<typename T>
+    [[nodiscard]] std::optional<T> get_variable_value(const ExecutionState<T>& state, std::string_view name) noexcept
     {
-        std::stringstream stream{source_text};
-        return interpret(stream, parameters);
+        if(const auto it = state._descriptor_table.find(std::string{name}); it != state._descriptor_table.end()) {
+            const auto[_, descriptor] = *it;
+
+            if(descriptor.is_constant()) {
+                return state.constants.at(descriptor.index()).value();
+            }
+            return state.variables.at(descriptor.index()).value();
+        }
+        return {};
+    }
+
+    namespace Interpreter {
+
+        template <std::floating_point T>
+        using ExecutionResult = std::variant<InterpreterErrorCode, ExecutionState<T>>;
+
+        template <std::floating_point T>
+        using ParameterMap = std::map<std::string, T>;
+
+        RAYCHELSCRIPT_INTERPRETER_API [[nodiscard]] ExecutionResult<double> interpret(const AST& ast, const ParameterMap<double>& parameters) noexcept;
+
+        [[nodiscard]] inline ExecutionResult<double>
+        interpret(std::istream& source_stream, const ParameterMap<double>& parameters) noexcept
+        {
+            const auto ast_or_error = Parser::parse(source_stream);
+            if (const auto* ec = std::get_if<Parser::ParserErrorCode>(&ast_or_error); ec) {
+                return InterpreterErrorCode::no_input;
+            }
+
+            return interpret(Raychel::get<AST>(ast_or_error), parameters);
+        }
+
+        [[nodiscard]] inline ExecutionResult<double>
+        interpret(const std::string& source_text, const ParameterMap<double>& parameters) noexcept
+        {
+            std::stringstream stream{source_text};
+            return interpret(stream, parameters);
+        }
     }
 
 } //namespace RaychelScript::Interpreter
--- copy_dlls.sh
+++ copy_dlls.sh
@@ -1,5 +1,6 @@
 BUILD_MODE=$1
+MODULE=$2
 
-cp build/*/$BUILD_MODE/*.dll ./build/Parser/test/$BUILD_MODE/
+cp build/*/$BUILD_MODE/*.dll ./build/$MODULE/test/$BUILD_MODE/
 
-cp "C:/Program Files (x86)/RaychelLogger/lib/RaychelLogger/RaychelLogger.dll" ./build/Parser/test/$BUILD_MODE
+cp "C:/Program Files (x86)/RaychelLogger/lib/RaychelLogger/RaychelLogger.dll" ./build/$MODULE/test/$BUILD_MODE
