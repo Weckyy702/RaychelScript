b5d14734af1b8a80d840ff97aa9461c713aff2dd
Weckyy702 <50154363+Weckyy702@users.noreply.github.com>
2021-12-29 14:22:48 +0100

    Updated testing executable
    You can now specify the script and its arguments

--- VM/test/main.test.cpp
+++ VM/test/main.test.cpp
@@ -7,46 +7,41 @@
 
 #include "RaychelCore/AssertingGet.h"
 
-int main()
+int main(int argc, char** argv)
 {
     using namespace RaychelScript::Pipes;
 
-    const auto ast_or_error = Lex{lex_file, "../../../shared/test/simple.rsc"} | Parse{};
+    std::string script_name{"../../../shared/test/abc.rsc"};
+    std::vector<double> args{1, 2};
 
-    if (log_if_error(ast_or_error)) {
-        return 1;
+    //NOLINTBEGIN(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+
+    if (argc > 2) {
+        script_name = argv[1];
+
+        args.clear();
+        args.reserve(argc - 2);
+        for (int i = 2; i < argc; i++) {
+            char* end{};
+            const double arg = std::strtod(argv[i], &end);
+            if (end != argv[i]) {
+                args.emplace_back(arg);
+            }
+        }
     }
 
-    const auto data_or_error = ast_or_error | Assemble{};
+    //NOLINTEND(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+
+    Logger::info("Executing script ", script_name, '\n');
+
+    const auto data_or_error = Lex{lex_file, script_name} | Parse{} | Assemble{};
 
     if (log_if_error(data_or_error)) {
         return 1;
     }
 
     const auto data = data_or_error.value();
-
-    Logger::log("Input identifiers:\n");
-    for (const auto& [identifier, address] : data.config_block.input_identifiers) {
-        Logger::info(address, " -> ", identifier, '\n');
-    }
-
-    Logger::log("Output identifiers:\n");
-    for (const auto& [identifier, address] : data.config_block.output_identifiers) {
-        Logger::info(address, " -> ", identifier, '\n');
-    }
-
-    Logger::log("Immediate values:\n");
-    for (const auto& [value, address] : data.immediate_values) {
-        Logger::info(address, " -> ", value, '\n');
-    }
-
-    Logger::log("Instructions:\n");
-    std::size_t index{0};
-    for (const auto instr : data.instructions) {
-        Logger::info(index++, ": ", instr, '\n');
-    }
-
-    const auto state_or_error = RaychelScript::VM::execute<double>(data_or_error.value(), {1, 2, 3});
+    const auto state_or_error = RaychelScript::VM::execute<double>(data, args);
 
     if (const auto* ec = std::get_if<RaychelScript::VM::VMErrorCode>(&state_or_error); ec) {
         Logger::log(*ec, '\n');
@@ -55,10 +50,15 @@ int main()
 
     const auto state = Raychel::get<RaychelScript::VM::VMState<double>>(state_or_error);
 
-    index = 0;
+    std::size_t index{};
 
     for (const auto& value : state.memory) {
         Logger::log('$', index++, ": ", value, '\n');
+    }
+
+    const auto output = RaychelScript::VM::get_output_variables(state, data);
+    for (const auto& [name, value] : output) {
+        Logger::info(name, " = ", value, '\n');
     }
 
     return 0;
