eecc827c32da917b6942eb0b5d8590e83c71427c
Weckyy702 <50154363+Weckyy702@users.noreply.github.com>
2022-02-13 01:45:05 +0100

    Assembler+shared: Add BasicAssemblingContext class
    This allows for extracting logic that is inherent to assembling and does not depend on what we are assembling

--- Assembler/include/Assembler/AssemblingContext.h
+++ Assembler/include/Assembler/AssemblingContext.h
@@ -28,51 +28,27 @@
 #ifndef RAYCHELSCRIPT_ASSEMBLER_CONTEXT_H
 #define RAYCHELSCRIPT_ASSEMBLER_CONTEXT_H
 
+#include "shared/rasm/BasicAssemblingContext.h"
 #include "shared/rasm/Instruction.h"
-
-#include <stack>
-#include <string>
-#include <unordered_map>
-#include <vector>
 
 namespace RaychelScript::Assembler {
 
-    class AssemblingContext
+    class AssemblingContext final : public Assembly::BasicAssemblingContext
     {
 
         using Immediates = std::vector<std::pair<double, Assembly::MemoryIndex>>;
 
-        struct ScopeData
-        {
-            std::vector<std::string> names;
-        };
-
     public:
         explicit AssemblingContext(std::vector<Assembly::Instruction>& instructions, Immediates& immediate_values)
-            : instructions_{instructions}, immediate_values_{immediate_values}
+            : Assembly::BasicAssemblingContext{1}, instructions_{instructions}, immediate_values_{immediate_values}
         {}
 
         /**
-        * \brief Return if the name has a memory index assigned to it
-        * 
-        * \param name name of a variable
-        * \return true The name has a memory index associated with it
-        * \return false The name does not have a memory index associated with it
+        * \brief Return the index for the reserved A register
         */
-        [[nodiscard]] bool has_name(const std::string& name) const noexcept
+        [[nodiscard]] static constexpr Assembly::MemoryIndex result_index() noexcept
         {
-            return names_.find(name) != names_.end();
-        }
-
-        /**
-        * \brief Return if the context has at least one scope on the stack
-        * 
-        * \return true The context has at least one scope on the stack
-        * \return false The context has no scopes on the stack
-        */
-        [[nodiscard]] bool has_scopes() const noexcept
-        {
-            return !scopes_.empty();
+            return Assembly::make_memory_index(0);
         }
 
         /**
@@ -90,29 +66,9 @@ namespace RaychelScript::Assembler {
         }
 
         /**
-        * \brief Return the memory index associated with a name
-        * 
-        * \param name Name of a variable
-        * \return Assembly::MemoryIndex memory index for the name
-        */
-        [[nodiscard]] Assembly::MemoryIndex memory_index_for(const std::string& name) const noexcept
-        {
-            RAYCHEL_ASSERT(has_name(name));
-            return names_.find(name)->second;
-        }
-
-        /**
-        * \brief Return the index for the reserved A register
-        */
-        [[nodiscard]] static constexpr Assembly::MemoryIndex result_index() noexcept
-        {
-            return Assembly::make_memory_index(0);
-        }
-
-        /**
         * \brief Return the index to the last instruction
         */
-        [[nodiscard]] auto instruction_index() const noexcept
+        [[nodiscard]] std::size_t instruction_index() const noexcept override
         {
             return instructions_.size() - 1;
         }
@@ -149,51 +105,6 @@ namespace RaychelScript::Assembler {
             return instruction_index();
         }
 
-        void push_scope() noexcept
-        {
-            scopes_.emplace();
-        }
-
-        void pop_scope() noexcept
-        {
-            RAYCHEL_ASSERT(!scopes_.empty());
-            const auto& scope = scopes_.top();
-
-            for (const auto& name : scope.names) {
-                names_.erase(name);
-            }
-
-            scopes_.pop();
-        }
-
-        /**
-        * \brief Allocate a new variable and return its memory index
-        * 
-        * \param name Name of the variable
-        */
-        [[nodiscard]] auto allocate_variable(const std::string& name) noexcept
-        {
-            const auto [did_insert, index] = _allocate_new(names_, name);
-            if (did_insert && !scopes_.empty()) {
-                scopes_.top().names.emplace_back(name);
-            }
-            return index;
-        }
-
-        /**
-        * \brief Allocate a new immediate value and return its memory index
-        * 
-        * \param value immediate value
-        */
-        [[nodiscard]] auto allocate_immediate(double value) noexcept
-        {
-            const auto [did_insert, index] = _allocate_new(immediates_, value);
-            if (did_insert) {
-                immediate_values_.emplace_back(std::make_pair(value, index));
-            }
-            return index;
-        }
-
         /**
         * \brief allocate a new intermediate value and return its memory index
         */
@@ -206,7 +117,7 @@ namespace RaychelScript::Assembler {
                 }
             }
 
-            auto idx = _new_index();
+            auto idx = new_index();
             intermediates_.emplace_back(false, idx);
             return idx;
         }
@@ -221,46 +132,16 @@ namespace RaychelScript::Assembler {
             }
         }
 
-        [[nodiscard]] auto number_of_memory_locations() const noexcept
+        void allocate_immediate_internal(bool did_insert, Assembly::MemoryIndex index, double value) noexcept override
         {
-            RAYCHEL_ASSERT(std::cmp_less_equal(current_index_, std::numeric_limits<std::uint8_t>::max()));
-            return static_cast<std::uint8_t>(current_index_);
+            if (did_insert)
+                immediate_values_.emplace_back(std::make_pair(value, index));
         }
 
     private:
-        /**
-        * \brief Return the memory index for the given value. If none is assigned yet, allocate a new one
-        * 
-        * \tparam Container Type of the container
-        * \tparam T value type of the container
-        * \param container container to search in
-        * \param value value to search for
-        */
-        template <typename Container, typename T = typename Container::value_type>
-        [[nodiscard]] std::pair<bool, Assembly::MemoryIndex> _allocate_new(Container& container, const T& value) noexcept
-        {
-            if (const auto it = container.find(value); it != container.end()) {
-                return {false, it->second};
-            }
-            const auto it = container.insert({value, _new_index()});
-            return {true, it.first->second};
-        }
-
-        [[nodiscard]] Assembly::MemoryIndex _new_index() noexcept
-        {
-            return Assembly::make_memory_index(current_index_++);
-        }
-
         std::vector<Assembly::Instruction>& instructions_;
         Immediates& immediate_values_;
         std::vector<std::pair<bool, Assembly::MemoryIndex>> intermediates_;
-
-        std::size_t current_index_{1}; //the first index is reserved for the A register
-
-        std::unordered_map<std::string, Assembly::MemoryIndex> names_;
-        std::unordered_map<double, Assembly::MemoryIndex> immediates_;
-
-        std::stack<ScopeData, std::vector<ScopeData>> scopes_;
     };
 
 } //namespace RaychelScript::Assembler
