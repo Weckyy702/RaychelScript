8324c38a8ffa4952f60eec4f31ccf6f4f701de41
Weckyy702 <50154363+Weckyy702@users.noreply.github.com>
2022-04-06 23:01:30 +0200

    NativeAssembler: Make the testing executable for the native assembler much more usable as an actual assembly generator

--- NativeAssembler/test/main.test.cpp
+++ NativeAssembler/test/main.test.cpp
@@ -1,20 +1,29 @@
+#include "NativeAssembler/NativeAssembler.h"
+
 #include "Assembler/AssemblerPipe.h"
 #include "Lexer/LexerPipe.h"
 #include "Parser/ParserPipe.h"
 
-#include "NativeAssembler/NativeAssembler.h"
+#include <fstream>
+#include <iostream>
 
-int main()
+int main(int argc, char** argv)
 {
+    if (argc != 3) {
+        Logger::error("Usage: ", argv[0], " <INPUT_FILE> <OUTPUT_FILE>\n");
+        return 1;
+    }
     using namespace RaychelScript::Pipes;
-    const auto data_or_error = Lex{lex_file, "./abc.rsc"} | Parse{} | Assemble{};
 
+    const auto data_or_error = Lex{lex_file, argv[1]} | Parse{} | Assemble{};
     if (log_if_error(data_or_error)) {
         return 1;
     }
+
     const auto data = data_or_error.value();
 
-    const auto ec = RaychelScript::NativeAssembler::assemble(data, RaychelScript::NativeAssembler::assemble_x86_64, std::cout);
+    std::ofstream output_stream{argv[2]};
+    const auto ec = assemble(data, RaychelScript::NativeAssembler::assemble_x86_64, output_stream);
 
     if (ec != RaychelScript::NativeAssembler::NativeAssemblerErrorCode::ok) {
         Logger::error(ec, '\n');
